fde142adef2f15e18081471b50f01bd424cfc677
gmagino <gmagino@icloud.com>
2019-12-05 13:59:23 -0800

    yeeted

--- README.md
+++
@@ -1,1 +0,0 @@
-# gmagino.github.io
--- script.js
+++
@@ -1,8 +0,0 @@
-var btn = document.getElementById("myButton");
-
-btn.addEventListener("click", myFunction);
-
-function myFunction(){
-  document.getElementById("myText").innerHTML = "Hooray! You've clicked the button.";
-  document.getElementById("myButton").style.backgroundColor = "#8e44ad";
-};
